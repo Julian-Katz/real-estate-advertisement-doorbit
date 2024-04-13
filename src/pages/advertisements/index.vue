39637762be8d21707dcdecf5c78170d036f1da4b
Julian Katz <juliankatz@gmx.de>
2024-04-13 18:07:34 +0200

    add text classes & move price position

--- src/pages/advertisements/index.vue
+++ src/pages/advertisements/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <h1>Immobilieninserate</h1>
+    <h1 class="text-h3 mb-3">Immobilieninserate</h1>
     <AdvertisementsFilter v-model="advertisements" />
     <v-row>
       <v-col v-for="ad in advertisements" :key="ad.id" cols="12" md="6" lg="4">
@@ -9,19 +9,19 @@
           <v-card-title>
             {{ ad.address }}
           </v-card-title>
-          <v-card-subtitle>
-            Preis:
-            {{
-              new Intl.NumberFormat("de-DE", {
-                style: "currency",
-                currency: "EUR",
-              }).format(ad.price)
-            }}
-          </v-card-subtitle>
           <v-card-text>
-            <p>Quadratmeter: {{ ad.squareMeters }} m²</p>
-            <p>Zimmer: {{ ad.numberOfRooms }}</p>
-            <p>Energieeffizienzklasse: {{ ad.energyEfficiencyClass }}</p>
+            <p class="text-body-1">
+              Preis:
+              {{
+                new Intl.NumberFormat("de-DE", {
+                  style: "currency",
+                  currency: "EUR",
+                }).format(ad.price)
+              }}
+            </p>
+            <p class="text-body-1">Quadratmeter: {{ ad.squareMeters }} m²</p>
+            <p class="text-body-1">Zimmer: {{ ad.numberOfRooms }}</p>
+            <p class="text-body-1">Energieeffizienzklasse: {{ ad.energyEfficiencyClass }}</p>
           </v-card-text>
         </v-card>
       </v-col>
